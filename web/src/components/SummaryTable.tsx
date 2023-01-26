323bae41cecf2dc252b47de9a4a66f99c600c93f
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2023-01-26 10:54:59 -0300

    feat(web): add empety habits fill on summaryTable

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,6 +1,11 @@
+import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning';
 import { HabitDay } from './HabitDay';
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+
+const summaryDates = generateDatesFromYearBeginning();
+const minSummaryDatesSize = 18 * 7;
+const amountOfDaysToFill = minSummaryDatesSize - summaryDates.length;
 
 export function SummaryTable() {
   return (
@@ -10,7 +15,7 @@ export function SummaryTable() {
           return (
             <div
               key={`${weekDay}-${i}`}
-              className='text-zinc-400 text-xl h-10 w-10 flex items-center justify-center font-bold'
+              className='text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center'
             >
               {weekDay}
             </div>
@@ -19,10 +24,19 @@ export function SummaryTable() {
       </div>
 
       <div className='grid grid-rows-7 grid-flow-col gap-3'>
-        <HabitDay />
-        <HabitDay />
+        {summaryDates.map((date) => {
+          return <HabitDay key={date.toString()} />;
+        })}
 
-        <HabitDay />
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+            return (
+              <div
+                key={i}
+                className='w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed'
+              />
+            );
+          })}
       </div>
     </div>
   );
