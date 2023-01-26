97e5916ace141ae7411210a799d38d92e81b2ba2
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2023-01-26 12:43:35 -0300

    feat(mobile): add Home screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import {
   useFonts,
   Inter_400Regular,
@@ -7,6 +7,7 @@ import {
   Inter_800ExtraBold,
 } from '@expo-google-fonts/inter';
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [isFontsLoaded] = useFonts({
@@ -19,28 +20,13 @@ export default function App() {
   if (!isFontsLoaded) return <Loading />;
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>
-        ME up App.tsx to start working on your app!
-      </Text>
+    <>
+      <Home />
       <StatusBar
         barStyle='light-content'
         backgroundColor='transparent'
         translucent
       />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-    fontFamily: 'Inter_800ExtraBold',
-  },
-});
---
+++ mobile/src/lib/dayjs.ts
@@ -0,0 +1,4 @@
+import dayjs from 'dayjs';
+import 'dayjs/local/pt-br';
+
+dayjs.locale('pt-br');
